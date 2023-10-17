9b490df6b28ff9c35a9dace8deb843aea87957f5
LeahChi <146640958+LeahChi@users.noreply.github.com>
2023-10-17 15:39:29 +0000

    switch and if

---
+++ week2_c_boot/jacket.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+ 
+ int a = 6;
+
+ switch(a - 1) //evaluates the statement
+ {
+    case 0: printf ("Option 0 has been selected\n");
+    break;
+
+    case 1:  printf ("Option 1 has been selected\n");
+    break;
+
+    case 2:  printf ("Option 2 has been selected\n");
+    break;
+
+    case 5:  printf ("Option 23422 has been selected\n");
+    break;
+ }
+
+ return 0;
+
+}

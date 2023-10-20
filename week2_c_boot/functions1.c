855103453c7b5e2e93d7e11a8ae528da9f34b675
LeahChi <146640958+LeahChi@users.noreply.github.com>
2023-10-20 16:15:39 +0000

    trying to iterate over A string , functions

---
+++ week2_c_boot/functions1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char word_input[200];
+    int counter = 0;
+    printf("enter word: ");
+    scanf("%s", word_input);
+    
+    while(word_input)
+    {
+        
+    }
+
+    printf("length of string: %s", word_input);
+
+    return 0;
+}

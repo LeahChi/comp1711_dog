20f1b71751c77319bbd97ef15e4dd4d614d887b1
LeahChi <146640958+LeahChi@users.noreply.github.com>
2023-12-01 16:57:44 +0000

    task3

---
+++ task3/FitnessDataSorter.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define the struct for the fitness data
+typedef struct {
+    char date[11];
+    char time[6];
+    int steps;
+} FitnessData;
+
+// Function to tokenize a record
+void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
+    char *ptr = strtok(record, &delimiter);
+    if (ptr != NULL) {
+        strcpy(date, ptr);
+        ptr = strtok(NULL, &delimiter);
+        if (ptr != NULL) {
+            strcpy(time, ptr);
+            ptr = strtok(NULL, &delimiter);
+            if (ptr != NULL) {
+                *steps = atoi(ptr);
+            }
+        }
+    }
+}
+
+
+int main() {
+
+    char filename[100];
+    printf("Please enter the name of the data file: ");
+    scanf("%s",filename);
+
+    FILE *input = fopen(filename, "r");
+        if (!input)
+        {
+            printf("Error: File could not be opened\n");
+            return 1;
+        }
+
+
+}

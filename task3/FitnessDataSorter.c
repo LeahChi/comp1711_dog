a3bc7432d3f56977ec103302c5c269bfca3a4a66
LeahChi <146640958+LeahChi@users.noreply.github.com>
2023-12-05 16:20:46 +0000

    task3

--- task3/FitnessDataSorter.c
+++ task3/FitnessDataSorter.c
@@ -9,6 +9,13 @@ typedef struct {
     int steps;
 } FitnessData;
 
+FitnessData file_data;
+    int records;
+    int no_of_loops = 0;
+
+    char line[100]; //string to hold data
+    char *sp; //string pointer
+
 // Function to tokenize a record
 void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
     char *ptr = strtok(record, &delimiter);
@@ -37,17 +44,45 @@ int main() {
     printf("Please enter the name of the data file: ");
     scanf("%s",filename);
 
-    FILE *input = fopen(filename, "r");
-        if (!input)
+    FILE *file = fopen(filename, "r");
+        if (!file)
         {
             printf("Error: invalid file\n");
             return 1;
         }
 
+    while(fgets(line, 100, file)!=NULL)//looping through each line in file
+    {
+        records++;
+    }
+    if (records == 0) //checks to see if file is empty
+    {
+        printf("Error: invalid file\n");
+        return 1;
+    }
+
     //invalid file if not in 2023-12-30,09:30,598 format
 
+    /* while(fgets(line, 100, file)!=NULL)//fgets to get one line of data
+    {
+        if(no_of_loops<3)
+        {
+        sp = strtok(line, ","); //stops when reaches comma
+        strcpy(file_data.date, sp);
+
+        sp = strtok(NULL, ",");
+        strcpy(file_data.time, sp);
+        
+        sp = strtok(NULL, ",");
+        file_data.steps = atoi(sp);// a to i(nt) ->str to int
+        
+        printf("\n%s/%s/%d", file_data.date,file_data.time,file_data.steps);
+        no_of_loops++;
+        }
+    }*/
 
 
-
+    fclose(file);
+    return 0;
 
 }
